d5574d0868ab61429fce57c1e35175671d5282b0
Ankit Meena <120910719+ankitismyname@users.noreply.github.com>
2024-09-07 22:38:50 +0530

    Update admin.py

--- freshorganic/Userdata/admin.py
+++ freshorganic/Userdata/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile,ShoppingCartItem
+from .models import UserProfile,ShoppingCartItem,ProductOrder
 
 # Register the UserProfile model with the admin site
 admin.site.register(UserProfile)
 admin.site.register(ShoppingCartItem)
-
-
-
+admin.site.register(ProductOrder)
